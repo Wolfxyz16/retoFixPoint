b4bb5b2812614cbdc81a46c843fa030cd5c0de99
enekovilamor <57180168+enekovilamor@users.noreply.github.com>
2021-10-27 09:06:55 +0200

    popup

---
+++ js/popup.js
@@ -0,0 +1,54 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+                
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+var formLogin=document.getElementById("login-form");
+var login=document.getElementById("login");
+var formRegistro=document.getElementById("signup-form");
+var registrarse=document.getElementById("signup");
+var boton=document.getElementById("btn");
+var input=document.getElementsByClassName("input");
+
+formLogin.style.display="none";
+login.style = ("background", "none");
+
+function loginMostrar(){
+ formRegistro.style.display="none";
+  formLogin.style.display="";
+  registrarse.style = ("background", "none");
+  login.style=("background", "#fff");
+};
+
+function registroMostrar(){
+  formRegistro.style.display="";
+  formLogin.style.display="none";
+  login.style = ("background", "none");
+  registrarse.style=("background", "#fff");
+};
+
+function vaciar(){
+  for(var i=0; i<input.length; i++){
+    input[i].value="";
+  }
+};
