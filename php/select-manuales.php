6b328f21cf4964b6b2b94836f461e23b02a8f885
Yeray <yerayliloaiza@gmail.com>
2021-10-28 10:26:14 +0200

    codificado manuales, faltan consultas anidadas

---
+++ php/select-manuales.php
@@ -0,0 +1,23 @@
+<?php
+    include('conexion.php');
+
+    $query = 'SELECT * FROM manuales';
+
+    try{
+        $consulta = $conexion->prepare($query);
+        $consulta -> execute();
+        $manuales = $consulta -> fetchAll();
+    } catch(PDOException $e) {
+        echo '<script>console.log("' . $e->getMessage() .'");</script>';
+    }
+
+    foreach ($manuales as $manual) {
+        echo '<tr>';
+        echo '<td>' . $manual['titulo'] . '</td>';
+        echo '<td>' . $manual['fichero'] . '</td>';
+        echo '<td>' . $manual['portada'] . '</td>';
+        echo '<td>' . $manual['cod_autor'] . '</td>';
+        echo '<td>' . $manual['aprobado'] . '</td>';
+        echo '<tr>';
+    }
+?>
