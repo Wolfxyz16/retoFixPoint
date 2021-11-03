a70b721a77ea6aaf169f9f940af6658a4ac2b19d
Ekaitz <ekaitzmcbo@gmail.com>
2021-11-03 12:44:43 +0100

    Redireccsao du login

--- php/validacionRegistro.php
+++ php/validacionRegistro.php
@@ -24,6 +24,8 @@
             include("conexion.php");
             $insert=$conexion->prepare('INSERT INTO usuarios (name, surname, mail, password) VALUES (:nombre, :apellido, :email, :contrasena)');
             $insert->execute( array('nombre'=>$nombre, 'apellido'=>$apellido, 'email'=>$email, 'contrasena'=>$contrasena));
+            header('Location: ../html/popup.html');
+            exit;
         }catch(PDOException $e) {
             echo '<script>console.log(' . $e->getMessage() . ')</script>';
         }    
