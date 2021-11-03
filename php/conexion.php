35f69d89d512539a186f2a6aea5584fed96b67ed
Yeray <yerayliloaiza@gmail.com>
2021-11-03 10:45:59 +0100

    cambiadas comillas simples por dobles

--- php/conexion.php
+++ php/conexion.php
@@ -5,7 +5,7 @@
     $password = '';
 
     try {
-        $conexion = new PDO('mysql:host=$server;dbname=$dbname' , $username , $password);
+        $conexion = new PDO("mysql:host=$server;dbname=$dbname" , $username , $password);
         echo '<script>console.log("Oye, que la base de datos funciona")</script>';
 
     } catch (PDOException $e) {
