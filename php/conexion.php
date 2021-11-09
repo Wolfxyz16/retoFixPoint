7c0640cd933d326ffce9ce269040b71b4d8e448a
Yeray <yerayliloaiza@gmail.com>
2021-11-09 13:14:41 +0100

    comentarios varios

--- php/conexion.php
+++ php/conexion.php
@@ -4,6 +4,14 @@
     $username = 'root';
     $password = '';
 
+    // fixpoint-database.cfhjlaahn7bu.us-east-1.rds.amazonaws.com:3306
+
+    // admin
+    // xfyRa83Nk73RqsMS
+
+    // fixpoint
+    // Fixpoint123
+
     try {
         $conexion = new PDO("mysql:host=$server;dbname=$dbname" , $username , $password);
         echo '<script>console.log("Oye, que la base de datos funciona")</script>';
