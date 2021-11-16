ea32714b804206d33c99495d207e64e82a836691
Yeray <yerayliloaiza@gmail.com>
2021-11-16 23:47:30 +0100

    meter manuales funciona

--- php/insertar/crear-manual.php
+++ php/insertar/crear-manual.php
@@ -1,4 +1,25 @@
 <?php
-    include('../conexion.php');
+    if ( isset($_REQUEST['submit']) ) {
+        include('../conexion.php');
+        
+        $manual = [
+            "titulo" => $_REQUEST['titulo'],
+            "fichero" => $_REQUEST['fichero'],
+            "portada" => $_REQUEST['portada'],
+            "cod_autor" => 1,
+            "aprobado" => 1
+        ];
+
+        $insert_manual = "INSERT INTO manuales (titulo,fichero,portada,cod_autor,aprobado) VALUES (:titulo,:fichero,:portada,:cod_autor,:aprobado)";
+
+        try {
+            $sentencia = $conexion -> prepare($insert_manual);
+            $sentencia -> execute($manual);
+            header("LOCATION: http://localhost:8010/retofixpoint/html/admin.php");
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+    }
 
 ?>
