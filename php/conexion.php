4ac1f84194a4b19df6275b25dca365ce93ccfb8f
Yeray <yerayliloaiza@gmail.com>
2021-11-16 09:42:27 +0100

    elimininado mensaje de exito en la coniexion de la base de datos

--- php/conexion.php
+++ php/conexion.php
@@ -16,7 +16,6 @@
 
     try {
         $conexion = new PDO("mysql:host=$server;port=$port;dbname=$dbname" , $username , $password);
-        echo '<script>console.log("Oye, que la base de datos funciona")</script>';
 
     } catch (PDOException $e) {
         echo '<script>console.log("' . $e->getMessage() . '")</script>';
