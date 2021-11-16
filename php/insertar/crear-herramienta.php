a2d67bd6f2f2cc7afde3a8a9c41c587c5a7ab362
Yeray <yerayliloaiza@gmail.com>
2021-11-16 22:23:44 +0100

    funciona basicamente

--- php/insertar/crear-herramienta.php
+++ php/insertar/crear-herramienta.php
@@ -1,4 +1,23 @@
 <?php
-    include('../conexion.php');
+    if ( isset($_REQUEST['submit']) ) {
+        include('../conexion.php');
+        
+        $herramienta = [
+            "nombre" => $_REQUEST['nombre'],
+            "marca_modelo" => $_REQUEST['marca_modelo'],
+            "foto" => $_REQUEST['foto'],
+            "disponibilidad" => "Disponible"
+        ];
+
+        $insert_herramienta = "INSERT INTO herramientas (nombre,marca_y_modelo,foto,disponibilidad) VALUES (:nombre, :marca_modelo, :foto, :disponibilidad)";
+
+        try {
+            $sentencia = $conexion -> prepare($insert_herramienta);
+            $sentencia -> execute($herramienta);
+            header("LOCATION: http://localhost:8010/retofixpoint/html/admin.php");
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 
 ?>
