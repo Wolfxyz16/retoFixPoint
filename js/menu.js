526be4d1044c153ae51934d9dfa98f941ccdb7ad
Ekaitz <ekaitzmcbo@gmail.com>
2021-11-16 11:50:31 +0100

    menu responsive

--- js/menu.js
+++ js/menu.js
@@ -1,49 +1,26 @@
 import {slider} from './slider.js';
 import {administrador} from './admin.js';
 import {descarga} from './descargaManuales.js';
-import{alquiler} from './alquilerHerramienta.js';
+import {alquiler} from './alquilerHerramienta.js';
+import {accesoDesdeIndex} from './accesoDesdeIndex.js';
+import {accesoDesdeHtml} from './accesoDesdeHtml.js';
 window.onload= function () {
-
-    var items = document.getElementsByClassName('item');
-    const sitios_web = ["", "html/biblioteca.php", "html/manuales.php", "html/sobre_nosotros.php"];
-    
-    for (let i = 0; i < items.length; i++) {
-        items[i].addEventListener('click', function () {
-            location.href = sitios_web[i];
-        });
-        
-    };
-    if(document.getElementById('admin')){
-        document.getElementById('modo-admin').addEventListener('click', function () {
-            location.href = "../html/admin.php";
-        });
-        document.getElementById('cerrar-sesion').addEventListener('click', function () {
-            location.href = "../html/inicio.php";
-            sessionStorage.removeItem('usuario');
-        });
-    }else if(document.getElementById('usuario')){
-        document.getElementById('cerrar-sesion').addEventListener('click', function () {
-            location.href = "../html/inicio.php";
-            sessionStorage.removeItem('usuario');
-        });
-    }else if(document.getElementById('iniciosesion')){
-        document.getElementById('iniciosesion').addEventListener('click', function () {
-            location.href = "../html/login.html";
-        });
+    console.log(window.location.pathname.indexOf("/html/"));
+    if (window.location.pathname.indexOf("/html/") >-1){
+       accesoDesdeHtml();
+    }else{
+       accesoDesdeIndex();
     }
-
     
     if (window.location.pathname.indexOf("/html/admin.php") >-1 ){
-
         administrador();
     }
     else if (window.location.pathname.indexOf("/html/manuales.php") >-1 ){
-       
         descarga();
     }
     else if (window.location.pathname.indexOf("/html/biblioteca.php") >-1 ){
         alquiler();
-    }else if(window.location.pathname.indexOf("") >-1){
+    }else if(window.location.pathname.indexOf("/html/sobre_nosotros.php")==-1){
         slider();
     }
 };
