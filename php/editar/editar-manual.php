93a2dbc39dc88ad1272df82020303fc870a88e61
Yeray <yerayliloaiza@gmail.com>
2021-11-17 12:55:46 +0100

    funcionalidad editar registros

---
+++ php/editar/editar-manual.php
@@ -0,0 +1,30 @@
+<?php
+    if ( isset($_REQUEST['submit']) ) {
+
+        include('../conexion.php');
+        
+        $manual = [
+            "id" => $_REQUEST['id'],
+            "titulo" => $_REQUEST['titulo'],
+            "portada" => $_REQUEST['portada'],
+            "fichero" => $_REQUEST['fichero']
+        ];
+        
+        $resultado_update = [
+            "error" => false,
+            "mensaje" => "Manual " . $manual['titulo'] . " editado con exito"
+        ];
+
+        $update_manual = "UPDATE manuales SET titulo=:titulo, portada=:portada, fichero=:fichero WHERE cod_manual=:id";
+
+        try {
+            $sentencia = $conexion -> prepare($update_manual);
+            $sentencia -> execute($manual);
+            header("LOCATION: http://localhost/retofixpoint/html/admin.php");
+        } catch (PDOException $e) {
+            $resultado['error'] = true;
+            $resultado['mensaje'] = $e->getMessage();
+        }
+
+    }
+?>
