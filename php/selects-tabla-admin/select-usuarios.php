8daf43ce2253bc8174a636e6ee842d80f54a5504
Yeray <yerayliloaiza@gmail.com>
2021-11-17 09:13:59 +0100

    enlaces a iconos editar borrar

--- php/selects-tabla-admin/select-usuarios.php
+++ php/selects-tabla-admin/select-usuarios.php
@@ -27,8 +27,8 @@
             echo '<td>' . $usuario['name'] . '</td>';
             echo '<td>' . $usuario['surname'] . '</td>';
             echo '<td>' . $usuario['mail'] . '</td>';
-            echo '<td><img src="../img/svg/settings.svg" alt="icono engranaje editar">';
-            echo '<img src="../img/svg/trash.svg" alt="icono cubo de basura"></td>';
+            echo '<td><a href="editar-registros/editar-usuario.php?id='. $usuario['cod_user'] . '"><img src="../img/svg/settings.svg" alt="icono engranaje editar"></a>';
+            echo '<a href="/editar-registros/editar-usuario.php?id="'. $usuario['cod_user'] . '"><img src="../img/svg/trash.svg" alt="icono cubo de basura"></a></td>';
         echo '<tr>';
     }
 
