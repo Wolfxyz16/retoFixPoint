76e207068c02dc1a7d5f09409e8be31604650ff2
Yeray <yerayliloaiza@gmail.com>
2021-11-17 21:04:42 +0100

    funciona borrar las fotos

--- php/borrar/borrar-herramienta.php
+++ php/borrar/borrar-herramienta.php
@@ -11,15 +11,33 @@
     } else {
         $resultado_borrar['error'] = true;
         $resultado_borrar['mensaje'] = "El id no corresponde a ninguna herramienta";
+        echo $resultado_borrar['mensaje'];
     }
+
+    $query = 'SELECT * FROM herramientas WHERE cod_herramienta =' . $id;
+
+    try{
+        $consulta = $conexion->prepare($query);
+        $consulta -> execute();
+        $herramientas = $consulta -> fetchAll();
+    } catch(PDOException $e) {
+        $resultado_borrar['error'] = true;
+        $resultado_borrar['mensaje'] = "Foto asociada no encontrada";
+        echo $resultado_borrar['mensaje'];
+    }
+
+    unlink("C:/xampp/htdocs/retoFixPoint/herramientas/".$herramientas[0]['foto']);
 
     $query = 'DELETE FROM herramientas WHERE cod_herramienta =' . $id;
 
     try{
         $consulta = $conexion->prepare($query);
         $consulta -> execute();
-        header('LOCATION: ../../html/admin.php');
     } catch(PDOException $e) {
-        echo '<script>console.log("' . $e->getMessage() .'");</script>';
+        $resultado_borrar['error'] = true;
+        $resultado_borrar['mensaje'] = "No se pudo borrar la foto";
+        echo $resultado_borrar['mensaje'];
     }
+
+    header('LOCATION: ../../html/admin.php');
 ?>
