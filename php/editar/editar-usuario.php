73445f2c48828087126a1f6405860f382037357e
Yeray <yerayliloaiza@gmail.com>
2021-11-17 09:31:25 +0100

    funciona editar usuario

---
+++ php/editar/editar-usuario.php
@@ -0,0 +1,30 @@
+<?php
+    if ( isset($_REQUEST['submit']) ) {
+
+        include('../conexion.php');
+        
+        $usuario = [
+            "id" => $_REQUEST['id'],
+            "nombre" => $_REQUEST['nombre'],
+            "apellido" => $_REQUEST['apellido'],
+            "mail" => $_REQUEST['mail']
+        ];
+        
+        $resultado_update = [
+            "error" => false,
+            "mensaje" => "Usuario " . $usuario['nombre'] . " editado con exito"
+        ];
+
+        $update_usuario = "UPDATE usuarios SET name=:nombre, surname=:apellido, mail=:mail WHERE cod_user=:id";
+
+        try {
+            $sentencia = $conexion -> prepare($update_usuario);
+            $sentencia -> execute($usuario);
+            header("LOCATION: http://localhost/retofixpoint/html/admin.php");
+        } catch (PDOException $e) {
+            $resultado['error'] = true;
+            $resultado['mensaje'] = $e->getMessage();
+        }
+
+    }
+?>
