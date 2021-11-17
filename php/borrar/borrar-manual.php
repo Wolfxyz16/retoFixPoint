2be19761eb9d19c4b31ef0258c65718e01a44265
Yeray <yerayliloaiza@gmail.com>
2021-11-17 21:16:51 +0100

    ahora elimina el fichero guardado

--- php/borrar/borrar-manual.php
+++ php/borrar/borrar-manual.php
@@ -13,13 +13,29 @@
         $resultado_borrar['mensaje'] = "El id no corresponde a ningun manual";
     }
 
+    $query = 'SELECT * FROM manuales WHERE cod_manual =' . $id;
+
+    try{
+        $consulta = $conexion->prepare($query);
+        $consulta -> execute();
+        $manuales = $consulta -> fetchAll();
+    } catch(PDOException $e) {
+        $resultado_borrar['error'] = true;
+        $resultado_borrar['mensaje'] = "No se encontro fichero ni la portada asociada";
+        echo $resultado_borrar['mensaje'];
+    }
+
+    unlink("C:/xampp/htdocs/retoFixPoint/manuales/archivos/".$manuales[0]['fichero']);
+    unlink("C:/xampp/htdocs/retoFixPoint/manuales/portadas/".$manuales[0]['portada']);
+
     $query = 'DELETE FROM manuales WHERE cod_manual =' . $id;
 
     try{
         $consulta = $conexion->prepare($query);
         $consulta -> execute();
-        header('LOCATION: ../../html/admin.php');
     } catch(PDOException $e) {
         echo '<script>console.log("' . $e->getMessage() .'");</script>';
     }
+
+    header('LOCATION: ../../html/admin.php');
 ?>
